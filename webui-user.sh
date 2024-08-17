09f0c24d50e2649655e2f3b26c1a6de1ed96de6f
MacMilling™ <155540467+MacMilling@users.noreply.github.com>
2024-08-17 21:37:59 +0200

    Update webui-user.sh
    Anpassung Bash-Script (CPU-Verarbeitung)

--- webui-user.sh
+++ webui-user.sh
@@ -4,13 +4,31 @@
 #########################################################
 
 # Install directory without trailing slash
-#install_dir="/home/$(whoami)"
+install_dir="/home/$(whoami)"
 
 # Name of the subdirectory
-#clone_dir="stable-diffusion-webui"
+clone_dir="stable-diffusion-webui"
+
+# Full installation path
+install_path="${install_dir}/${clone_dir}"
+
+echo "Initialisiere MacMilling Skript (www.macmilling.de)"
+echo "sd-webui path: $install_path"
+
+export SADTALKER_CHECKPOINTS="${install_path}/models/SadTalker/checkpoints"
+export COMMANDLINE_GENERAL_ARGS="--api --api-log --allow-code --skip-torch-cuda-test"
+export COMMANDLINE_SERVER_ARGS="--listen --port 7969"
+export COMMANDLINE_OPT_ARGS="--use-cpu all --no-half --no-half-vae"
+export COMMANDLINE_CUSTOM_ARGS="--update-check --enable-insecure-extension-access"
+export COMMANDLINE_AUTH_ARGS="--gradio-allowed-path ${install_path} --gradio-auth macmilling:litlithammer"
+export COMMANDLINE_DEBUG_ARGS="" #--reinstall-torch --reinstall-xformers"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="${COMMANDLINE_DEBUG_ARGS} ${COMMANDLINE_GENERAL_ARGS} ${COMMANDLINE_SERVER_ARGS} ${COMMANDLINE_OPT_ARGS} ${COMMANDLINE_AUTH_ARGS} ${COMMANDLINE_CUSTOM_ARGS}"
+echo "start-parameter: $COMMANDLINE_ARGS"
+
+export IIB_CACHE_DIR ="${install_path}/cache/ibb/"
+export IIB_SECRET_KEY=none
 
 # python3 executable
 #python_cmd="python3"
@@ -19,13 +37,13 @@
 #export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+venv_dir="venv"
 
 # script to launch to start the app
 #export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+#export TORCH_COMMAND="pip install -U torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
