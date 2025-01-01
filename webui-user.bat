5a37b6deb6672f132cce665e30f17038ed909b0b
Silver <65376327+silveroxides@users.noreply.github.com>
2025-01-01 06:25:32 +0100

    Add old repo reference to webui-user.bat
    Added so that user can install dev branch separately easily and reference old repo.

--- webui-user.bat
+++ webui-user.bat
@@ -5,4 +5,16 @@ set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
 
+@REM Uncomment following code to reference an existing A1111 checkout.
+@REM set A1111_HOME=
+@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+@REM --models-dir  %A1111_HOME%/models ^
+@REM --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
+@REM --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
+@REM --embeddings-dir %A1111_HOME%/embeddings ^
+@REM --lora-dir %A1111_HOME%/models/Lora ^
+
+@REM Uncomment following code to set venv to the old checkout.
+@REM set VENV_DIR=%A1111_HOME%\\venv
+
 call webui.bat
