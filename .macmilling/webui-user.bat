de724ceb310f16a2af197157f43500b9860bdfae
MacMilling™ <155540467+MacMilling@users.noreply.github.com>
2025-02-23 12:22:15 +0100

    Add files via upload

--- .macmilling/webui-user.bat
+++ .macmilling/webui-user.bat
@@ -13,33 +13,34 @@ if "%*"=="--server" (
 
 echo ^.^.^.
 
-REM set NVCC_FLAGS=-allow-unsupported-compiler
+set NVCC_FLAGS=-allow-unsupported-compiler
 set TF_ENABLE_ONEDNN_OPTS=0
 set PYTHON=
-set GIT=
+set GIT=git
 set VENV_DIR=
+set SADTALKER_CHECKPOINTS=G:\stable-diffusion-webui\models\SadTalker\checkpoints
 set COMMANDLINE_ARGS= %COMMANDLINE_ARGS%
 
 REM #### CUSTOM ARGUMENTS ####
 REM #------------------------#
 set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --api --allow-code
-set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --medvram
-set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --upcast-sampling --opt-split-attention
-set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --no-half
-set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --disable-safe-unpickle
-set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --xformers
-REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --enable-insecure-extension-access
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --upcast-sampling 
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --enable-insecure-extension-access
 REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --listen
 REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --nowebui
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --opt-split-attention
 REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --disable-all-extensions 
 REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --disable-extra-extensions
 REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --reinstall-torch --reinstall-xformers
-REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --no-download-sd-model
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --xformers
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --no-download-sd-model
 REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --gradio-img2img-tool color-sketch
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --disable-safe-unpickle
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --medvram --log-startup
 REM #
 REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --log-startup
 
-set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
+REM set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
 REM set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:768
 
 REM #
